733a1ba3fff0d87bdda8ecb980ba8e673d2d7e54
user <user@this.com>
2019-09-30 15:46:56 -0400

    replaced jwtutil

--- src/main/java/com/training/api/authenticate/TokenAPI.java
+++ src/main/java/com/training/api/authenticate/TokenAPI.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/account")
 public class TokenAPI {
-	JWTUtil jwtUtil = new JWTMockUtil();
+	JWTUtil jwtUtil = new JWTHelper();
 	@PostMapping("/token")
 	public ResponseEntity<?> getToken(@RequestBody TokenRequestData tokenRequestData) throws IOException {
 		
