1007153f809c10cb85a0544ec5fea55ceef9637b
user <user@this.com>
2019-10-01 16:01:38 -0400

    added customer class

---
+++ src/main/java/com/training/api/authenticate/Customer.java
@@ -0,0 +1,34 @@
+package com.training.api.authenticate;
+
+public class Customer {
+	long id;
+	String name;
+	String email;
+	String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+}
